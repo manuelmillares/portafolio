7e45605511b68363447f4e61056e2bd95ce3cd51
Manuel Millares <manuelmillares1@gmail.com>
2021-11-17 19:24:25 +0100

    Add projects component

---
+++ src/data.js
@@ -0,0 +1,54 @@
+export const projects = [
+    {
+        title: "Ecommerce Dashbaord",
+        subtitle: "React and ElasticSearch",
+        description:
+            "Set of metrics, KPIs and other data that’s designed to highlight, in the clearest possible way, trends, anomalies and information related to an online store.",
+        image: "./project-1.gif",
+        link: "https://reactbootcamp.com",
+    },
+    {
+        title: "Ecommerce Dashbaord",
+        subtitle: "React and HighChart and Material",
+        description:
+            "Software solution to improve data representation using libraries such as Higchart and Material and consuming data from graphql api",
+        image: "./project-2.gif",
+        link: "https://reedbarger.com",
+    },
+    {
+        title: "Professional Portfolio",
+        subtitle: "React and Tailwind CSS",
+        description:
+            "Collection of work samples and other documents that provide an overview of your abilities and qualifications.",
+        image: "./project-3.gif",
+        link: "https://jsbootcamp.com",
+    }
+];
+
+
+export const skills = ["React", "JavaScript", "Angular", "TypeScript", "Material UI"]
+
+
+export const testimonials = [
+    {
+        quote: "Excelente desarrollador",
+        name: "Jose Manuel Espinosa",
+        company: "Treew",
+        image: "./project-1.gif",
+    }, {
+        quote: "Excelente desarrollador",
+        name: "Alejandro Mustelier",
+        company: "Treew",
+        image: "./project-1.gif",
+    }, {
+        quote: "Excelente desarrollador",
+        name: "Boris",
+        company: "Treew",
+        image: "./project-1.gif",
+    }, {
+        quote: "Excelente desarrollador",
+        name: "William Vasquez",
+        company: "Treew",
+        image: "./project-1.gif",
+    },
+]
